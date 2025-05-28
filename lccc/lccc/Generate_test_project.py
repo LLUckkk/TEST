05d94effe24afc62cf82767cce639ae7d8b82199
Zhanghanwen <zhanghanwen@buaa.edu.cn>
2025-05-28 15:27:06 +0800

    [feat]zhw:add to 576

--- lccc/lccc/Generate_test_project.py
+++ lccc/lccc/Generate_test_project.py
@@ -203,7 +203,7 @@ def fill_template(template_path, replacements):
 
 def solve(dataset: Dict[str, Dict]) -> Dict[str, List[Dict]]:
     cnt = 1
-    index = 569
+    index = 575  # 该576了
 
     for item_key in dataset:
         if cnt < index:
